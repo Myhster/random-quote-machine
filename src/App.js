7d7a22f51dd4e9eff5f2e4d29f3af8ab4efbad8e
Your Name <wertzuio987@web.de>
2021-09-28 16:14:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React, { useState, useEffect } from 'react';
+import COLORS_ARRAY from './colorsArray';
+
+let quoteDBUrl =
+  'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
 function App() {
+  const [quote, setQuote] = useState('Love Live');
+  const [author, setAuthor] = useState('Alo');
+  const [number, setNumber] = useState(0);
+  const [quotesArray, setQuotesArray] = useState(null);
+  const [accentColor, setAccentColor] = useState('#4ca2f9');
+
+  const fetchQuotes = async (url) => {
+    const response = await fetch(url);
+    const parsedJSON = await response.json();
+    setQuotesArray(parsedJSON.quotes);
+    console.log(parsedJSON);
+  };
+
+  useEffect(() => {
+    fetchQuotes(quoteDBUrl);
+  }, []);
+
+  const setQuoteandAuthor = () => {
+    setQuote(quotesArray[number].quote);
+    setAuthor(quotesArray[number].author);
+    let randInt = Math.floor(Math.random() * quotesArray.length);
+    setNumber(randInt);
+    let colorNumber = Math.floor(Math.random() * COLORS_ARRAY.length);
+    setAccentColor(COLORS_ARRAY[colorNumber]);
+  };
+  let styling = { backgroundColor: accentColor };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id='top' className='App'>
+      <header id='header' className='App-header' style={styling}>
+        <div id='quote-box'>
+          <p id='text' style={{ color: accentColor }}>
+            "{quote}"
+          </p>
+          <p id='author' style={{ color: accentColor }}>
+            -{author}
+          </p>
+
+          <button
+            id='new-quote'
+            onClick={setQuoteandAuthor}
+            style={{ backgroundColor: accentColor }}
+          >
+            get inspired
+          </button>
+          <a
+            style={{ backgroundColor: accentColor }}
+            id='tweet-quote'
+            href={encodeURI(
+              'http://twitter.com/intent/tweet?text=${quote}-${author}'
+            )}
+          >
+            tweet
+          </a>
+        </div>
       </header>
     </div>
   );
